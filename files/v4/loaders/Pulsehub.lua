6ba9681efc91900c7dd522cedd7bfa07f80e1e3b
XenScriptt <janjanc438@gmail.com>
2026-09-14 00:15:01 +0800

    Update Pulsehub.lua

--- files/v4/loaders/Pulsehub.lua
+++ files/v4/loaders/Pulsehub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjVmZWM0MTlmLWNkZGQtNGMwMS04Zjg3LTQyMWJlOTlkMTQ1MCIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/WTXVzPS8uW/0"))()
